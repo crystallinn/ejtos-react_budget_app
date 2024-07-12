c60a5079b8a827cff7e725d66a0d7bc4fa3298b7
crystallinn <crystal.orc@gmail.com>
2024-07-12 12:19:34 -0700

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,16 +1,17 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
-const ExpenseTotal = () => {
-    const { expenses, currency } = useContext(AppContext);
+
+const Remaining = () => {
+    const { expenses, budget, currency } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
+        return (total = total + item.cost);
     }, 0);
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
     return (
-        <div className='alert alert-primary'>
-            <span>Spent so far: {currency}{totalExpenses}</span>
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: {currency}{budget - totalExpenses}</span>
         </div>
     );
 };
-export default ExpenseTotal;
-
+export default Remaining;
