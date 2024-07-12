2e8244c37623cc7667ce1ca8c770d915b8f04069
crystallinn <crystal.orc@gmail.com>
2024-07-12 12:18:14 -0700

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -18,7 +18,7 @@ const Currency = () => {
      return (
           <div className='alert alert-secondary'>
           <label style={{marginLeft: '1rem' , backgroundColor:'green', color:'white'}} >Currency
-        <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'#33FF49', color:'white'}}>
+        <select name='hover_color'id="currency" onChange={(event)=>changeCurrency(event.target.value)} style={{ marginLeft: '1rem' , backgroundColor:'green', color:'white'}}>
           <option style={{color:'black'}} value="£">£ Pound</option>
           <option style={{color:'black'}} value="$">$ Dollar</option>
           <option style={{color:'black'}} value="€">€ Euro</option>
